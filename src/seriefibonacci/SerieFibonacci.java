7dcd04d7127f0055c8e2a07845aac4765b1084d8
mariovilchis <mariovilchis01@hotmail.com>
2026-02-25 11:29:20 -0600

    NumeroPrimo

--- src/seriefibonacci/SerieFibonacci.java
+++ src/seriefibonacci/SerieFibonacci.java
@@ -5,23 +5,27 @@ import java.util.Scanner;
 public class SerieFibonacci {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
+        Scanner sc = new Scanner(System.in); // Objeto para leer datos del usuario
+        
         System.out.println("Ingresa el límite:");
-        int limite = sc.nextInt();
+        int limite = sc.nextInt(); // Cantidad de números a mostrar
 
-        int a = 0;
-        int b = 1;
+        int a = 0; // Primer número de la serie
+        int b = 1; // Segundo número de la serie
 
         System.out.print("Serie: ");
 
+        // Se ejecuta según el límite ingresado
         for (int i = 0; i < limite; i++) {
-            System.out.print(a + " ");
+            
+            System.out.print(a + " "); // Imprime el número actual
 
-            int siguiente = a + b;
-            a = b;
-            b = siguiente;
+            int siguiente = a + b; // Calcula el siguiente número
+            a = b;                 // El segundo pasa a ser el primero
+            b = siguiente;         // El nuevo valor pasa a ser el segundo
         }
 
-        sc.close();
+        sc.close(); // Cierra el Scanner
     }
 }
